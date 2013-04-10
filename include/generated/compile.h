e47ffad05810c1246c82dd7ef384983d62c7f357
Cheongwol <ik__ot__pk@naver.com>
2013-04-10 23:18:31 +0900

    Make For VastoSKT Kenrel Source

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Wed Mar 28 16:51:34 KST 2012"
+#define LINUX_COMPILE_TIME "16:51:34"
+#define LINUX_COMPILE_BY "soori"
+#define LINUX_COMPILE_HOST "iu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
